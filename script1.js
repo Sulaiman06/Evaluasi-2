9750e265df3afd743cc1715f1c74a10ffcc64c4d
Sulaiman06 <sulaimanabdulwahhab07@gmail.com>
2022-07-23 10:33:20 +0700

    Evaluasi

---
+++ script1.js
@@ -0,0 +1,140 @@
+for( x = 1; x < 10; x++ ) {
+    for( y = 1; y < 10; y++ ) {
+        if( x == y || x + y == 10 || y == 5 || x == 5 ) {
+            document.write(` + `)
+        } else {
+            document.write(` ~ `)
+        }
+    }
+    document.write(`<br>`)
+}
+
+document.write(`<br>`)
+
+for( x = 1; x < 10; x++ ) {
+    for( y = 1; y < 10; y++ ) {
+        if( y + x >= 10 ) {
+            document.write(` + `)
+        } else {
+            document.write(` ~ `)
+        }
+    }
+    document.write(`<br>`)
+}
+
+document.write(`<br>`)
+
+for( x = 1; x < 10; x++ ) {
+    for( y = 1; y < 10; y++ ) {
+        if( x + y <= 10 && x >= y || x + y >= 10 && x <= y ) {
+            document.write(` + `)
+        } else {
+            document.write(` ~ `)
+        }
+    }
+    document.write(`<br>`)
+}
+
+document.write(`<br><br><br><br>`)
+
+for( x = 1; x < 10; x++ ) {
+    for( y = 1; y < 10; y++ ) {
+        if( x == 1 || y == 1 && x < 6 || x == 5 || y == 9 && x > 5 || x == 9 ) {
+            document.write(` + `)
+        } else {
+            document.write(` ~ `)
+        }
+    }
+    document.write(`<br>`)
+}
+
+document.write(`<br>`)
+
+for( x = 1; x < 10; x++ ) {
+    for( y = 1; y < 10; y++ ) {
+        if( x == 9 || y == 1 || y == 9 ) {
+            document.write(` + `)
+        } else {
+            document.write(` ~ `)
+        }
+    }
+    document.write(`<br>`)
+}
+
+document.write(`<br>`)
+
+for( x = 1; x < 10; x++ ) {
+    for( y = 1; y < 10; y++ ) {
+        if( y == 1 || x == 9 && y < 7 ) {
+            document.write(` + `)
+        } else {
+            document.write(` ~ `)
+        }
+    }
+    document.write(`<br>`)
+}
+
+document.write(`<br>`)
+
+for( x = 1; x < 10; x++ ) {
+    for( y = 1; y < 10; y++ ) {
+        if( x == 1 || x == 5 || y ==9 || y == 1 ) {
+            document.write(` + `)
+        } else {
+            document.write(` ~ `)
+        }
+    }
+    document.write(`<br>`)
+}
+
+document.write(`<br>`)
+
+for( x = 1; x < 10; x++ ) {
+    for( y = 1; y < 10; y++ ) {
+        if( y == 5 ) {
+            document.write(` + `)
+        } else {
+            document.write(` ~ `)
+        }
+    }
+    document.write(`<br>`)
+}
+
+document.write(`<br>`)
+
+for( x = 1; x < 10; x++ ) {
+    for( y = 1; y < 10; y++ ) {
+        if( y == 1 || y == 9 || x == y && x < 6 || x + y == 10 && x < 6 ) {
+            document.write(` + `)
+        } else {
+            document.write(` ~ `)
+        }
+    }
+    document.write(`<br>`)
+}
+
+document.write(`<br>`)
+
+for( x = 1; x < 10; x++ ) {
+    for( y = 1; y < 10; y++ ) {
+        if( x == 1 || x == 5 || y ==9 || y == 1 ) {
+            document.write(` + `)
+        } else {
+            document.write(` ~ `)
+        }
+    }
+    document.write(`<br>`)
+}
+
+document.write(`<br>`)
+
+for( x = 1; x < 10; x++ ) {
+    for( y = 1; y < 10; y++ ) {
+        if( x == y || y ==9 || y == 1 ) {
+            document.write(` + `)
+        } else {
+            document.write(` ~ `)
+        }
+    }
+    document.write(`<br>`)
+}
